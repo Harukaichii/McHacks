4ad91a74352021feccac1723a217209ee682e41c
Kiaein <kiaein.kishigami@gmail.com>
2019-02-03 03:01:39 -0500

    added styles

--- src/components/Result.js
+++ src/components/Result.js
@@ -56,8 +56,8 @@ class Result extends React.Component {
         <div>
           {this.state.showModal ? (
             <Modal>
-              <button onClick={this.props.toggleModal}>X</button>
-              <p>hi</p>
+              <button id="xbutton" onClick={this.props.toggleModal}>X</button>
+              <p>hi this is a whole lot of text because texts are cool and necessary to test and see if the Boxes are a sufficient size and i think writing stuff is really nice even though were about to pass out but thats ok because im just testing the box size lol see writing this is pretty easy!</p>
             </Modal>
           ) : null}
         </div>
